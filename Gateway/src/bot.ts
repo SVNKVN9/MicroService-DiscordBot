f81450d7079f0b6bb1092b537e78af885fe7af4e
SVNKVN9 <sundaynight.1919@gmail.com>
2023-03-10 21:12:01 +0700

    Add event filter in bot.ts

--- Gateway/src/bot.ts
+++ Gateway/src/bot.ts
@@ -1,6 +1,7 @@
 import { Client, GatewayIntentBits } from 'discord.js'
 import { GatewayDispatchEvents } from 'discord-api-types/v10'
 import { IConfig } from './types'
+import axios from 'axios'
 
 const { Service_URL, TOKEN } = require('../../config.json') as IConfig
 
@@ -12,8 +13,19 @@ const client = new Client({
     ]
 })
 
-client.on('raw', (data) => {
+const MessageService = async (path: string, data: any) => await axios.post(`${Service_URL.Message}/${path}`, data)
 
+client.on('raw', (data) => {
+    switch (data.t) {
+        case GatewayDispatchEvents.Ready:
+            console.log(`(${client.shard?.ids}) is ready`)
+
+            break;
+        case GatewayDispatchEvents.MessageCreate:
+            MessageService('create', data.d)
+
+            break;
+    }
 })
 
 client.login(TOKEN)
